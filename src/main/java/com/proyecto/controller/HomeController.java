4a6ac5301eb7d47491c0718d615e0ab2e5972ef6
BENNER SIMEON <benner.simeon@gmail.com>
2025-10-25 21:55:56 -0500

    Actualizacion de HomeController

--- src/main/java/com/proyecto/controller/HomeController.java
+++ src/main/java/com/proyecto/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  @Controller
 public class HomeController {
 	
-	 @GetMapping("/")
+	 @GetMapping("/barberia")
 	    public String home() {
 	        return "index"; // nombre del archivo Thymeleaf
 	 }
