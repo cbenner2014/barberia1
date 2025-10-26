a8f9e28ff526c3f9199fedc2b1b7d9eb2d2132da
BENNER SIMEON <benner.simeon@gmail.com>
2025-10-25 23:24:43 -0500

    Configura H2 para tests

--- src/test/java/com/proyecto/BarberiaApplicationTests.java
+++ src/test/java/com/proyecto/BarberiaApplicationTests.java
@@ -2,12 +2,17 @@ package com.proyecto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class BarberiaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
