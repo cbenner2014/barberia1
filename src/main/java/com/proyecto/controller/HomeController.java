ea93e882bd3be341423d82f4430a39963d8a4200
BENNER SIMEON <benner.simeon@gmail.com>
2025-10-25 23:53:47 -0500

    actualizacion de Index2

--- src/main/java/com/proyecto/controller/HomeController.java
+++ src/main/java/com/proyecto/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 	
 	 @GetMapping("/")
 	    public String home() {
-	        return "index"; // nombre del archivo Thymeleaf
+	        return "Index2"; // nombre del archivo Thymeleaf
 	 }
 }
